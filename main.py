1daa973fa20341ad477b85cfb5c53222a7753f6d
qqpayne <ivanakostelov@gmail.com>
2020-11-11 10:05:29 +0300

    Missed some commits

---
+++ main.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+import re
+import json
+import argparse
+from scrapers import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument('domain', type=str, help="Domain name to search for subdomains")
+parser.add_argument('output', type=str, help="Name of output file")
+parser.add_argument('-k', type=str, help="Path to JSON file with API keys. Currently only VirusTotal and SecurityTrail.")
+parser.add_argument('-v', action="count", help="Verbosity (use twice if you want to see everything)")
+parser.add_argument("-t", action='store_true', help="Use Google Certificate Transparency for additional SSL scraping (VERY SLOW, usually adds 50-100 entries)")
+#parser.add_argument("-d", action='store_true', help="Use reverse DNS to find subdomains (slow)")
+#parser.add_argument("-h", action='store_true', help="Use HTML parsing to find subdomains (VERY SLOW!)")
+
+APISources = [VirusTotal, SecurityTrails]
+
+def validate(domain):
+	try:
+
+	# all sources accept kaspersky.com, not https://kaspersky.com
+	if args.domain.startswith("http"):
+		domain = re.findall(r'[^/]*\.[a-zA-Z]{2,}$', args.domain)[0]
+	else:
+		domain = args.domain
+	return domain
+	
+	except:
+		print("Enter valid domain")
+		exit(1)
+
+def work(domain):
+	sources = [child for child in Scraper.__subclasses__() if child not in APISources]
+	if not args.t:
+		sources.remove(GoogleTransparency)
+
+	subdomains = [[] for i in range(len(sources))]
+	threads = [sources[i](domain, subdomains[i], verbose) for i in range(len(sources))]
+	if args.k:
+		with open(args.k, "r") as f:
+			keys = json.loads(f.read())
+
+		for src in APISources:
+			if str(src.__name__) in keys:
+				if verbose > 0:
+					print("[+] Found %s key" % src.__name__)
+				subdomains.append([])
+				threads.append(src(domain, subdomains[len(subdomains)-1], verbose, keys[src.__name__]))
+
+	for thread in threads:
+		thread.start()
+	for thread in threads:
+		thread.join()
+
+	return subdomains
+
+def merge(subdomains):
+	overall = []
+	for output in subdomains:
+		overall += output
+	return set(overall)
+
+def beautify(output):
+	to_delete = set()
+	for entry in output:
+		if entry.startswith(".") or entry.startswith("*"):
+			to_delete.add(entry)
+	output -= to_delete
+
+
+def main():
+	domain = validate(args.domain)
+	subdomains = work(domain)
+	overall = merge(subdomains)
+	beautify(overall)
+
+	with open(args.output, "w+") as f:
+		for subdomain in overall:
+			f.write("%s\n" % subdomain)
+
+	print("\nFinished. There are %d subdomains for %s" % (len(overall), domain))
+
+if __name__ == "__main__":
+	args = parser.parse_args()
+	verbose = 0 if (args.v is None) else verbose = args.v
+	main()
+
+
+
+
---
+++ scrapers.py
@@ -0,0 +1,307 @@
+import re
+import time
+import random
+from threading import Thread
+import json
+import requests
+
+class Scraper(Thread):
+
+	def __init__(self, url, domain, subdomains, pages=False, verbosity=0):
+		Thread.__init__(self)
+		self.domain = domain
+		self.subdomains = subdomains
+		self.url = url
+		self.verbosity = verbosity
+		self.pages = pages
+		self.ses = requests.Session()
+
+	def run(self):
+		self.scrape()
+		if self.verbosity > 0:
+			print("[*] Found %d subdomains on %s" % (len(self.subdomains), self.__class__.__name__))
+		
+	def request(self, query, page):
+		url = self.url.format(query=query, page=page)
+		try:
+			response = self.ses.get(url)
+		except:
+			return 0
+		return response
+
+	def extractURLs(self, response):
+		response = response.text
+		try:
+			subdoms = re.findall(r'[\w\.]+\.%s' % self.domain, response)
+		except:
+			return 0
+
+		return subdoms
+
+	def wait(self):
+		time.sleep(random.randint(1, 5))
+		return
+
+	def query(self):
+		if self.pages:
+			visited = []
+			for item in self.subdomains:
+				if item not in visited and len(visited) <= 10:
+					visited.append(item)
+			visited = ' -'.join(visited)
+			query = "site:%s -www.%s %s" % (self.domain, self.domain, visited)
+		else:
+			query = "%s" % self.domain
+
+		return query
+
+	def paging(self, page):
+		return page+10
+
+	def scrape(self):
+		page = 0
+		retries = 0
+
+		while True:
+			query = self.query()
+			try:
+				response = self.request(query, page)
+				newUrls = self.extractURLs(response)
+			except:
+				return
+
+			if self.verbosity > 1:
+				for i in range(len(newUrls)):
+					print("%s, found on %s" % (newUrls[i], self.__class__.__name__))
+
+			if self.pages:
+				# If our source have pages, then check if new querry gave us new urls
+				# If it don't, we stop our search. Else - wait and proceed.
+
+				if retries >= 5 or len(newUrls) == 0:
+					self.subdomains += newUrls
+					return
+
+				unique = 0
+				for newurl in newUrls:
+					if newurl not in self.subdomains:
+						unique += 1
+				self.subdomains += newUrls
+
+				if unique == 0:
+					page = self.paging(page)
+					retries += 1
+
+				self.wait()
+			else:
+				self.subdomains += newUrls
+				return
+
+class Google(Scraper):
+
+	def __init__(self, domain, subdomains, verbosity):
+		url = "https://google.com/search?q={query}&start={page}"
+		pages = True
+		super().__init__(url, domain, subdomains, pages, verbosity)
+
+class Yahoo(Scraper):
+
+	def __init__(self, domain, subdomains, verbosity):
+		url = 'https://search.yahoo.com/search?p={query}&b={page}'
+		pages = True
+		super().__init__(url, domain, subdomains, pages, verbosity)
+
+class Baidu(Scraper):
+
+	def __init__(self, domain, subdomains, verbosity):
+		url = 'https://www.baidu.com/s?pn={page}&wd={query}&oq={query}'
+		pages = True
+		super().__init__(url, domain, subdomains, pages, verbosity)
+
+class DuckDuckGo(Scraper):
+
+	def __init__(self, domain, subdomains, verbosity):
+		url = 'https://duckduckgo.com/d.js?q={query}&p={page}'
+		pages = True
+		super().__init__(url, domain, subdomains, pages, verbosity)
+
+class RapidDNS(Scraper):
+
+	def __init__(self, domain, subdomains, verbosity):
+		url = 'https://rapiddns.io/subdomain/{query}?full=1'
+		super().__init__(url, domain, subdomains, verbosity=verbosity)
+
+class crtDOTsh(Scraper):
+
+	def __init__(self, domain, subdomains, verbosity):
+		url = "https://crt.sh/?q={query}"
+		super().__init__(url, domain, subdomains, verbosity=verbosity)
+
+class GoogleTransparency(Scraper):
+
+	def __init__(self, domain, subdomains, verbosity):
+		url = "https://transparencyreport.google.com/transparencyreport/api/v3/httpsreport/ct/certsearch{query}{page}"
+		super().__init__(url, domain, subdomains, verbosity=verbosity)
+
+	def query(self):
+		query = "?include_subdomains=true&domain=%s" % self.domain
+		return query
+
+	def paging(self, response):
+		paging = "/page?p=%s" % response[3][1]
+		return paging
+
+	def extractURLs(self, response):
+		subdoms = [response[1][i][1] for i in range(len(response[1]))]
+		return subdoms
+
+	def scrape(self):
+		query = self.query()
+		response = self.request(query, '')
+		lastPage = json.loads(response.text[7:-2])[3][4]
+
+		while True:
+			try:
+				response = json.loads(response.text[7:-2])	
+				newUrls = self.extractURLs(response)
+			except:
+				return
+
+			if self.verbosity > 1:
+				for i in range(len(newUrls)):
+					print("%s, found on %s" % (newUrls[i], self.__class__.__name__))
+
+			self.subdomains += newUrls
+			page = self.paging(response)
+			# page num equals last page num
+			if (page is None) or (response[3][3] == lastPage):
+				return
+			response = self.request('', page)
+
+
+class CertSpotter(Scraper):
+
+	def __init__(self, domain, subdomains, verbosity):
+		url = "https://api.certspotter.com/v1/issuances?domain={query}&include_subdomains=true&expand=dns_names"
+		super().__init__(url, domain, subdomains, verbosity=verbosity)
+
+	def extractURLs(self, response):
+		subdoms = []
+		json = response.json()
+		for entry in json:
+			subdoms += entry["dns_names"]
+		return subdoms
+
+class SiteDossier(Scraper):
+
+	def __init__(self, domain, subdomains, verbosity):
+		url = "http://www.sitedossier.com/parentdomain/{query}"
+		super().__init__(url, domain, subdomains, verbosity=verbosity)
+
+class ThreatMiner(Scraper):
+
+	def __init__(self, domain, subdomains, verbosity):
+		url = 'https://www.threatminer.org/getData.php?e=subdomains_container&q={query}&t=0&rt=10&p=1'
+		super().__init__(url, domain, subdomains, verbosity=verbosity)
+
+
+class AlienVault(Scraper):
+
+	def __init__(self, domain, subdomains, verbosity):
+		url = 'https://otx.alienvault.com/otxapi/indicator/domain/passive_dns/{query}'
+		super().__init__(url, domain, subdomains, verbosity=verbosity)
+
+	def extractURLs(self, response):
+		subdoms = []
+		json = response.json()['passive_dns']
+		return [json[i]['hostname'] for i in range(len(json))]
+
+class ThreatCrowd(Scraper):
+
+	def __init__(self, domain, subdomains, verbosity):
+		url = "https://www.threatcrowd.org/searchApi/v2/domain/report/?domain={query}"
+		super().__init__(url, domain, subdomains, verbosity=verbosity)	
+
+	def extractURLs(self, response):
+		return response.json()['subdomains']
+
+class HackerTarget(Scraper):
+
+	def __init__(self, domain, subdomains, verbosity):
+		url = 'https://hackertarget.com/find-dns-host-records/'
+		super().__init__(url, domain, subdomains, verbosity=verbosity)
+
+	def request(self, query, page):
+		cookie = self.validateCookie()
+		data = {'theinput':self.domain, 'thetest':"hostsearch", "name_of_nonce_field":cookie, "_wp_http_referer":"/find-dns-host-records/"}
+		headers = {'Referer':self.url}
+		try:
+			response = self.ses.post(self.url, data=data, headers=headers)
+		except:
+			return 0
+		return response
+
+	def validateCookie(self):
+		req = self.ses.get(self.url)
+		return re.findall(r'name_of_nonce_field" value="(\w+)"', req.text)[0]
+
+class DNSDumpster(Scraper):
+
+	def __init__(self, domain, subdomains, verbosity):
+		url = 'https://dnsdumpster.com/'
+		super().__init__(url, domain, subdomains, verbosity=verbosity)
+
+	def request(self, query, page):
+		cookie = self.validateCookie()
+		cookies = {'csrftoken':cookie}
+		data = {'csrfmiddlewaretoken':cookie, 'targetip':self.domain}
+		headers = {'Referer':self.url}
+		try:
+			response = self.ses.post(self.url, cookies=cookies, data=data, headers=headers)
+		except:
+			return 0
+		return response
+
+	def validateCookie(self):
+		req = self.ses.get(self.url)
+		return re.findall(r'csrfmiddlewaretoken" value="(\w+)"', req.text)[0]
+
+class SecurityTrails(Scraper):
+
+	def __init__(self, domain, subdomains, verbosity, apikey):
+		url = 'https://api.securitytrails.com/v1/domain/{query}/subdomains'
+		self.apikey=apikey
+		super().__init__(url, domain, subdomains, verbosity=verbosity)
+
+	def request(self, query, page):
+		headers = {'apikey':self.apikey}
+		url = self.url.format(query=query, page=page)
+		try:
+			response = self.ses.get(url, headers=headers)
+		except:
+			return 0
+		return response
+
+	def extractURLs(self, response):
+		response = response.json()
+		return [response['subdomains'][i]+"."+self.domain for i in range(len(response['subdomains']))]
+
+class VirusTotal(Scraper):
+
+	def __init__(self, domain, subdomains, verbosity, apikey):
+		url = 'https://www.virustotal.com/api/v3/domains/{query}/subdomains'
+		self.apikey=apikey
+		super().__init__(url, domain, subdomains, verbosity=verbosity)
+
+	def request(self, query, page):
+		headers = {'x-apikey':self.apikey}
+		url = self.url.format(query=query, page=page)
+		try:
+			response = self.ses.get(url, headers=headers)
+		except:
+			return 0
+		return response
+
+	def extractURLs(self, response):
+		response = response.json()
+		return [response['data'][i]['id'] for i in range(len(response['data']))]
